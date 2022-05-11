8223665e5b3945b33c3a33ceb9377f3f6d95599f
Hugo Maia <87488505+hugo2m@users.noreply.github.com>
2022-05-11 01:09:03 -0300

    Update questao-12.sql
    #1

--- Tarefas/questao-12.sql
+++ Tarefas/questao-12.sql
@@ -1,3 +1,4 @@
+#12 Faça uma consulta que selecione o nome do projeto, data de início e fim do projeto, a descrição da atividade do projeto, data de início e fim da atividade.
 
 SELECT p.nome, p.datainicio, p.datafim, a.descricao, a.datainicio, a.datafim
 FROM atividade a, projeto p
