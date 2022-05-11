7a405cf3022cb739947b455e975cf6f25208b9f3
Hugo Maia <87488505+hugo2m@users.noreply.github.com>
2022-05-10 22:09:56 -0300

    Update questao-02.sql
    #1

--- Tarefas/questao-02.sql
+++ Tarefas/questao-02.sql
@@ -1,7 +1,10 @@
    # Q2 Faça uma consulta que selecione o nome de todos os funcionários, exceto o mais idoso.
-   
-   
-     select nome from funcionario
-     WHERE dtnasc > (
-     SELECT min(dtnasc) FROM funcionario
+     
+     select nome from funcionario          
+     WHERE dtnasc > (                      
+     SELECT min(dtnasc) FROM funcionario   
      )
+
+#linha 3    seleciona nome da tabela funcionario
+#linha 4    onde compara se dtnasc é maior 
+#linha 5    seleciona o menor dtnasc da tabela funcionario
