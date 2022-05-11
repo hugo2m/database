7e59ca07aaf650f9e5e70e0f1b77ba707a40519c
Hugo Maia <87488505+hugo2m@users.noreply.github.com>
2022-05-10 22:15:40 -0300

    Update questao-06.sql
    #1

--- Tarefas/questao-06.sql
+++ Tarefas/questao-06.sql
@@ -3,3 +3,7 @@
 SELECT f.nome, f.salario, d.descricao 
 from funcionario as f, departamento as d
 WHERE f.codDepto = d.codigo
+
+#Linha 3 seleciona o nome, salario e descrição da tabela funcionario
+#Linha 4 chama a tabela funcionario de f e a departamento de d
+#Linha 5 condição paara exibição onde codDepto tem que ser igual ao d.codigo
