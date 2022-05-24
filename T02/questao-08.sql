30404918c373790441c6f4bae86d8319dfdd63f0
Hugo Maia <87488505+hugo2m@users.noreply.github.com>
2022-05-24 01:01:16 -0300

    Update questao-08.sql
    #2

--- T02/questao-08.sql
+++ T02/questao-08.sql
@@ -1,3 +1,7 @@
+/*Crie um gatilho que delete todos os clientes de um voo sempre que aquele voo for deletado e 
+mais um gatilho para deletar o piloto. (Para efeito de informação, considere que o avião caiu).
+*/
+
 #Trigger
 CREATE TRIGGER deleteTodosPassageiros BEFORE
 DELETE on voo 
